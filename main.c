29c79630e1cf68b0a814cd68f6ade452499d2504
Luxusio <44326048+Luxusio@users.noreply.github.com>
2019-11-12 17:31:35 +0900

    Update main.c

--- main.c
+++ main.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
 
-#define MAX 100
-#define SWAP(a, b) int n=a;a=b;b=n;
+#define SIZE 100
+#define SWAP(a, b) int t=a;a=b;b=t;
 
 void main() {
-
-	int arr[MAX];
+	int arr[SIZE];
 	int len;
 	int i;
-
+	
 	scanf_s("%d", &len);
 
 	for (i = 0; i < len; i++) {
@@ -16,8 +15,12 @@ void main() {
 	}
 	
 	for (i = 1; i < len-1; i += 2) {
-		if (arr[i-1] < arr[i + 1])	{ SWAP(arr[i], arr[i + 1]); }
-		else						{ SWAP(arr[i], arr[i - 1]); }
+		if (arr[i+1] > arr[i] && arr[i + 1] > arr[i - 1]) {
+			SWAP(arr[i], arr[i + 1]);
+		}
+		else if (arr[i - 1] > arr[i] && arr[i - 1] > arr[i + 1]) {
+			SWAP(arr[i], arr[i - 1]);
+		}
 	}
 
 	for (i = 0; i < len; i++) {
