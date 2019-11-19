98b1a738aa362f1624859fb94792663776aba6e9
Luxusio <44326048+Luxusio@users.noreply.github.com>
2019-11-19 17:16:55 +0900

    Update 2091119 MoveZero.c

--- 2091119 MoveZero.c
+++ 2091119 MoveZero.c
@@ -3,25 +3,25 @@
 void main() {
 	int arr[] = { 6, 0, 8, 2, 3, 0, 4, 0, 1 };
 	int length = sizeof(arr) / sizeof(int);
-	int* result = (int*)malloc(sizeof(arr));
 
-	int top = 0;
 	int zeroCount = 0;
 	int i;
 
 	for (i = 0; i < length; i++) {
 		if (arr[i] == 0) {
-			result[length - ++zeroCount] = 0;
+			zeroCount++;
 		}
 		else {
-			result[top++] = arr[i];
+			arr[i - zeroCount] = arr[i];
 		}
 	}
 	
-
 	// prints arr
-	for (i = 0; i < length; i++) {
-		printf("%d, ", result[i]);
+	for (i = 0; i < length-zeroCount; i++) {
+		printf("%d, ", arr[i]);
+	}
+	for (i = 0; i < zeroCount; i++) {
+		printf("0, ");
 	}
 	printf("\n");
 
